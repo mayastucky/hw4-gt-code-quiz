205f2dc5fdd4c711aeba7a2591674aee8a9a0d41
Maya Stucky <mayaatstucky@gmail.com>
2020-06-11 15:26:44 -0400

    got the buttons to advance to the next question and establish a correct answer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const optionsEl = document.querySelector("#options");
 const questionPageEl = document.querySelector("#quiz");
 var timerEl = document.querySelector("#timer");
 const allButtonsEl = document.querySelectorAll(".buttonOpt");
+var yesOrNoEl = document.querySelector("#correctPlaceholder");
 
 // allButtonsEl.addEventListener("click", function(){
 //   generateQuestions();
@@ -17,41 +18,76 @@ startButtonEl.addEventListener("click", function (event) {
   generateQuestions();
   setTimer();
 });
-questionIndex = 0;
+
+// optionsEl.addEventListener("click", function () {
+//   generateQuestions();
+// });
+
+//starts the index of the object at position 0 to display the first question when the start button is clicked
+var questionIndex = 0;
 // This function of the code was inspired code from https://github.com/lbernadel/Code-Quiz
 function generateQuestions() {
+  //makes this page show up !
   questionPageEl.style.display = "block";
-  //starts the index of the object at position 0 to display the first question when the start button is clicked
   //this defines the correct answer as the answer stored in "an" in the object at index 0
   correctAnswer = questions[questionIndex].an;
   //this updates the stored text from the HTML to display the question in the object at index 0
   questionEl.textContent = questions[questionIndex].q;
   optionsEl.innerHTML = "";
   //this helps define and set what array the options are supposed to be pulled out of so they aren't pulled out of the next array every single time
+
   var choicesFromArray = questions[questionIndex].option;
+
   //optionsEl.textContent = questions[questionIndex].option;
   // console.log(optionsEl);
+  //questionIndex = questionIndex + 1;
+
   for (var i = 0; i < 4; i++) {
-    questionIndex = questionIndex + 1;
     console.log("create button!");
     var choicesButton = document.createElement("button");
+    //this variable choicesFromArray is assigned to whatever the options are at whatever spot questionIndex is currently at
+    //var choicesFromArray = questions[questionIndex].option;
+    console.log(choicesFromArray);
     //sets the button text to be whatever the options from the choicesFromArray were
     choicesButton.innerHTML = choicesFromArray[i];
     choicesButton.setAttribute("class", "btn btn-primary buttons");
     choicesButton.style.margin = "20px";
     console.log(choicesButton);
-    buttons = optionsEl.append(choicesButton);
+    optionsEl.append(choicesButton);
+
+    choicesButton.addEventListener("click", function (event) {
+      console.log("button clicked!");
+      console.log(questionIndex);
+      if (this.innerHTML === questions[questionIndex].an) {
+        yesOrNoEl.innerHTML = "Correct!";
+      } else {
+        secondsLeft = secondsLeft - 10;
+        yesOrNoEl.innerHTML = "Nope! -10 seconds";
+      }
+      questionIndex++;
+      console.log(questionIndex);
+      console.log(this.innerHTML);
+      generateQuestions();
+    });
   }
 }
+
+// function quizOver() {
+//   if (questionIndex === -1) {
+//     alert("You have completed the quiz!");
+//   }
+// }
+// quizOver();
+
 //TIMER TIME!
 secondsLeft = 75;
 function setTimer() {
   var timerInterval = setInterval(function () {
-    console.log("Timer set");
+    // console.log("Timer set");
     secondsLeft--;
     timerEl.textContent = " " + secondsLeft;
     if (secondsLeft === 0) {
-      clearInterval();
+      clearInterval(timerInterval);
     }
   }, 1000);
 }
